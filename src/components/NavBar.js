8f4c16f484a9e69fd634914f336bce4be8779f91
Nazareth-1 <nazarethbriseno@gmail.com>
2023-02-03 22:53:24 -0600

    Collapsable Nav Bar on Smaller Screen

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,34 @@
+import { useState, useEffect } from "react";
 import styles from "./NavBar.module.css"
 export default function NavBar(){
+    const [navBarCollapsed, openNavBar] = useState(false);
+    function toggleNavBar(){
+        openNavBar(!navBarCollapsed);
+    }
+
+    useEffect(() => {
+        function handleResize() {
+            if (window.innerWidth > 800) {
+                openNavBar(false);
+            }
+        }
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [openNavBar]);
+
     return (
         <nav>
             <h1>
                 BlogSpot
             </h1>
-            <ul>
+            <ul className={navBarCollapsed ? styles.navOpen : null}>
                 <li>Home</li>
                 <li>My Profile</li>
                 <li>Trending</li>
             </ul>
-            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <svg onClick={toggleNavBar} xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
             </svg>
         </nav>
